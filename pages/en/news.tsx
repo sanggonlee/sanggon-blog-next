055f125d6e65959b151a3b6a393098b7437fead9
Sang-gon Lee <lee.sg16@gmail.com>
2022-02-12 09:28:11 -0500

    Create news.tsx

---
+++ pages/en/news.tsx
@@ -0,0 +1,20 @@
+import Layout from '../../components/Layout';
+import PostList from '../../components/PostList';
+import { enNavigationLinks } from '../../lib/navigation';
+import { getPostItems } from '../../lib/posts';
+
+export default function EnNews({ postItems }) {
+  return (
+    <Layout lang="en" navigationLinks={enNavigationLinks}>
+      <PostList postItems={postItems} />
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      postItems: getPostItems({ lang: 'en', category: 'news' }),
+    },
+  };
+}
