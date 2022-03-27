e8499b83b0679874a51c99878df60d8df4075894
Sang-gon Lee <lee.sg16@gmail.com>
2022-03-27 10:17:24 -0400

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,7 +4,7 @@ type DatedObject = {
 
 export function byDescendingDate(a: DatedObject, b: DatedObject): number {
   if (!a.date || !b.date) {
-    return -1;
+    return 1;
   }
   return b.date < a.date ? -1 : 1;
 }
