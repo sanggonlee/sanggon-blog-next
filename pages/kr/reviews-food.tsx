1beb7e43a3e4df3600d0c2aa49e0e5a08276fde4
Sang-gon Lee <lee.sg16@gmail.com>
2022-03-27 10:13:50 -0400

    Update reviews-food.tsx

--- pages/kr/reviews-food.tsx
+++ pages/kr/reviews-food.tsx
@@ -4,6 +4,12 @@ import { krNavigationLinks } from '../../lib/navigation';
 
 const reviews = [
   {
+    title: 'Hamachi Nabe (You Don Ya)',
+    location: '108 Dundas St W, Toronto, ON',
+    rating: 3.5,
+    text: `그런대로 맛있었음. 달군 냄비에 밥과 하마치 사시미를 올린 다음에 달짝지근한 소스를 끼얹어서 먹는다. 물론 사시미는 얼마 안가서 바로 익는데 밥과의 조화가 썩 훌륭한 느낌은 아니었다.`,
+  },
+  {
     title: 'Donkotsue Ramen (Ikkousha Ramen)',
     location: '249 Queen St W, Toronto, ON',
     rating: 4.5,
